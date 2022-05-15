d7cdcb7905da30e7d583dcd70bbcbdb71d2d08d3
ooligors <ooligors@gmail.com>
2022-05-16 01:21:24 +0300

    js-hw-06

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,8 +1,7 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
-
+t
 function createBoxes(amount) {
   divBoxes.innerHTML = "";
   // destroyBoxes();
