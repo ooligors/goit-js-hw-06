8e8930c14fdbfb835f954346e803084315390808
ooligors <ooligors@gmail.com>
2022-05-24 16:38:26 +0300

    HW 6 -changes

--- js/task-09.js
+++ js/task-09.js
@@ -8,6 +8,11 @@ const spanEl = document.querySelector(".color");
 function changeColorBtn() {
 
   bodyEl.style.backgroundColor = getRandomHexColor();
-  spanEl.style.color = bodyEl.style.backgroundColor;
+  // spanEl.style.color = bodyEl.style.backgroundColor;
+  spanEl.textContent = getRandomHexColor();
+  console.log(bodyEl.style.backgroundColor);
+  console.log(getRandomHexColor());
+  console.log(spanEl);
+  console.log(spanEl.textContent);
 }
 buttonEl.addEventListener("click", changeColorBtn);
